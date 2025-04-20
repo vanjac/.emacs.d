bb295eea62defbc8aa4592cd209de4e4e9f5598e
j <chroma@chroma.zone>
2025-04-19 23:35:10 -0700

    Change font to Iosevka

--- init.el
+++ init.el
@@ -81,7 +81,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Iosevka Fixed" :foundry "UKWN" :slant normal :weight regular :height 128 :width normal)))))
 
 ;; Autoloaded files
 (require 'dired-x)
