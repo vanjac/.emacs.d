a2d154ef8b2f5cc0c0222fe9cb32d4a16c086a34
j <chroma@chroma.zone>
2025-07-14 08:15:52 -0700

    Update Replete command

--- init.el
+++ init.el
@@ -104,17 +104,18 @@
 (setq replete-command
       (list
        "node"
-        (expand-file-name "~/code/Replete/replete.js")
-        "--browser_port=9325"
-        "--content_type=js:text/javascript"
-        "--content_type=mjs:text/javascript"
-        "--content_type=css:text/css"
-        "--content_type=html:text/html; charset=utf-8"
-        "--content_type=wasm:application/wasm"
-        "--content_type=woff2:font/woff2"
-        "--content_type=svg:image/svg+xml"
-        "--content_type=png:image/png"
-        "--content_type=webp:image/webp"))
+       (expand-file-name "~/code/Replete/replete.js")
+	"--browser_hostname=0.0.0.0"
+	"--browser_port=9325"
+	"--content_type=js:text/javascript"
+	"--content_type=mjs:text/javascript"
+	"--content_type=css:text/css"
+	"--content_type=html:text/html; charset=utf-8"
+	"--content_type=wasm:application/wasm"
+	"--content_type=woff2:font/woff2"
+	"--content_type=svg:image/svg+xml"
+	"--content_type=png:image/png"
+	"--content_type=webp:image/webp"))
 
 ;; Mode overrides:
 (push '("\\.m?js\\'" . js-ts-mode) auto-mode-alist)
