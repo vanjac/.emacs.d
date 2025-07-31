e81acfbbe2179a6ad4b61398024325bfa5eb21d3
j <chroma@chroma.zone>
2025-07-31 16:27:35 -0700

    Change font size

--- init.el
+++ init.el
@@ -89,7 +89,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka Fixed" :foundry "UKWN" :slant normal :weight regular :height 128 :width normal)))))
+ '(default ((t (:family "Iosevka Fixed" :foundry "UKWN" :slant normal :weight regular :height 120 :width normal)))))
 
 ;; Autoloaded files
 (require 'dired-x)
@@ -178,7 +178,7 @@
     (apply orig args)))
 
 ;; Skeletons:
-(define-skeleton jsdoc-skeleton "Insert JSDoc multiline comment" nil
+(define-skeleton jsdoc-skeleton "Insert JSDoc comment" nil
   "/**" _ "*/")
 (define-skeleton js-defun-skeleton "Insert JavaScript function with JSDoc" nil
   "/**" ?\n
