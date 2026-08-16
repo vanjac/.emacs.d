e4a7171741da437176851efee8f5af9180fe476c
j <chroma@chroma.zone>
2026-08-16 12:08:02 -0700

    Speed up startup

--- early-init.el
+++ early-init.el
@@ -1,3 +1,9 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Speed up startup
+(setopt gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setopt gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))))
+
 (setopt custom-enabled-themes '(modus-operandi))
