3076634c830012198bb4a8534ef41fa2036ee280
j v <vantjac@gmail.com>
2026-09-01 18:13:46 +0000

    Add powershell file extension

--- init.el
+++ init.el
@@ -112,6 +112,7 @@
 (push '("\\.go\\'" . go-ts-mode) auto-mode-alist)
 (push '("CMakeLists\\.txt\\'" . cmake-ts-mode) auto-mode-alist)
 (push '("\\.cmake\\'" . cmake-ts-mode) auto-mode-alist)
+(push '("\\.ps1\\'" . prog-mode) auto-mode-alist)
 
 ;;; Commands:
 (defun duplicate-forward (n)
